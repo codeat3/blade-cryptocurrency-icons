8ad0fff46f063e603eec9000b5a22d44029dca0e
Swapnil Sarwe <swapnilsarwe@gmail.com>
2021-05-12 00:28:46 +0530

    fixes the icon set name

--- src/BladeCryptocurrencyIconsServiceProvider.php
+++ src/BladeCryptocurrencyIconsServiceProvider.php
@@ -17,7 +17,7 @@ final class BladeCryptocurrencyIconsServiceProvider extends ServiceProvider
         $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
             $config = $container->make('config')->get('blade-cryptocurrency-icons', []);
 
-            $factory->add('codicon', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
+            $factory->add('cryptocurrency-icons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
         });
 
     }
