fb4bfd0ea086e0d989eeb79337b3b9c06fa53299
swapnilsarwe <swapnilsarwe@users.noreply.github.com>
2022-07-26 05:12:48 +0000

    Fix styling

--- src/BladeCryptocurrencyIconsServiceProvider.php
+++ src/BladeCryptocurrencyIconsServiceProvider.php
@@ -17,24 +17,24 @@ final class BladeCryptocurrencyIconsServiceProvider extends ServiceProvider
         $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
             $config = $container->make('config')->get('blade-cryptocurrency-icons', []);
 
-            $factory->add('cryptocurrency-icons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
+            $factory->add('cryptocurrency-icons', array_merge(['path' => __DIR__ . '/../resources/svg'], $config));
         });
     }
 
     private function registerConfig(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/blade-cryptocurrency-icons.php', 'blade-cryptocurrency-icons');
+        $this->mergeConfigFrom(__DIR__ . '/../config/blade-cryptocurrency-icons.php', 'blade-cryptocurrency-icons');
     }
 
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../resources/svg' => public_path('vendor/blade-cryptocurrency-icons'),
+                __DIR__ . '/../resources/svg' => public_path('vendor/blade-cryptocurrency-icons'),
             ], 'blade-cri'); // TODO: updating this alias to `blade-cryptocurrency-icons` in next major release
 
             $this->publishes([
-                __DIR__.'/../config/blade-cryptocurrency-icons.php' => $this->app->configPath('blade-cryptocurrency-icons.php'),
+                __DIR__ . '/../config/blade-cryptocurrency-icons.php' => $this->app->configPath('blade-cryptocurrency-icons.php'),
             ], 'blade-cryptocurrency-icons-config');
         }
     }
